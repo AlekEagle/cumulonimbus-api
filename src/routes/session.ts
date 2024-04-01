f460235b6a7aa6e435f7212680ab06c69b595218
Alek Evans <github@alekeagle.com>
2024-04-01 12:11:33 -0800

    add ability to rename sessions and change scoped session permissions

--- src/routes/session.ts
+++ src/routes/session.ts
@@ -27,6 +27,7 @@ import SessionPermissionChecker, {
 } from '../middleware/SessionPermissionChecker.js';
 import ReverifyIdentity from '../middleware/ReverifyIdentity.js';
 import isType from '../utils/TypeAsserter.js';
+import KVExtractor from '../utils/KVExtractor.js';
 
 import { Request, Response } from 'express';
 import Bcrypt from 'bcrypt';
@@ -39,6 +40,7 @@ logger.debug('Loading: Session Routes...');
 app.post(
   // POST /api/login
   '/api/login',
+  KillSwitch(KillSwitches.ACCOUNT_LOGIN),
   AutoTrim(),
   BodyValidator({
     username: new ExtendedValidBodyTypes('string', true),
@@ -54,7 +56,6 @@ app.post(
     windowMs: 60 * 1000, // 1 minute
     max: 3,
   }),
-  KillSwitch(KillSwitches.ACCOUNT_LOGIN),
   async (
     req: Request<
       null,
@@ -179,11 +180,12 @@ app.post(
 );
 
 app.post(
-  // POST /api/session
+  // POST /api/users/me/sessions
   '/api/users/me/sessions',
+  KillSwitch(KillSwitches.ACCOUNT_MODIFY),
+  KillSwitch(KillSwitches.ACCOUNT_LOGIN),
   ReverifyIdentity(),
   AutoTrim(),
-  KillSwitch(KillSwitches.ACCOUNT_MODIFY),
   SessionPermissionChecker(PermissionFlags.SESSION_CREATE),
   BodyValidator({
     name: new ExtendedValidBodyTypes('string', true),
@@ -443,12 +445,204 @@ app.get(
   },
 );
 
+app.put(
+  // PUT /api/users/me/sessions/:sid/name
+  '/api/users/me/sessions/:sid([0-9]{10})/name',
+  KillSwitch(KillSwitches.ACCOUNT_MODIFY),
+  SessionChecker(),
+  SessionPermissionChecker(PermissionFlags.SESSION_MODIFY),
+  BodyValidator({
+    name: 'string',
+  }),
+  async (
+    req: Request<{ sid: string }, null, { name: string }>,
+    res: Response<
+      Cumulonimbus.Structures.Session | Cumulonimbus.Structures.Error
+    >,
+  ) => {
+    // Find the session with the given ID.
+    const session = await Session.findOne({
+      where: {
+        user: req.user.id,
+        id: req.params.sid,
+      },
+    });
+
+    // If no session was found, return an InvalidSession error.
+    if (!session) return res.status(404).json(new Errors.InvalidSession());
+
+    // Update the session's name.
+    await session.update({
+      name: req.body.name,
+    });
+
+    logger.debug(
+      `User ${req.user.username} (${req.user.id}) requested to rename their session ${session.id} to ${req.body.name}.`,
+    );
+
+    // Return a success.
+    return res.status(200).json(KVExtractor(session.toJSON(), ['user'], true));
+  },
+);
+
+app.put(
+  // PUT /api/users/:uid/sessions/:sid/name
+  '/api/users/:uid([0-9]{13})/sessions/:sid([0-9]{10})/name',
+  SessionChecker(true),
+  SessionPermissionChecker(PermissionFlags.STAFF_MODIFY_SESSIONS),
+  BodyValidator({
+    name: 'string',
+  }),
+  async (
+    req: Request<{ uid: string; sid: string }, null, { name: string }>,
+    res: Response<
+      Cumulonimbus.Structures.Session | Cumulonimbus.Structures.Error
+    >,
+  ) => {
+    try {
+      // Find the user with the given ID.
+      const user = await User.findByPk(req.params.uid);
+
+      // If no user was found, return an InvalidUser error.
+      if (!user) return res.status(404).json(new Errors.InvalidUser());
+
+      // Find the session with the given ID.
+      const session = await Session.findOne({
+        where: {
+          user: user.id,
+          id: req.params.sid,
+        },
+      });
+
+      // If no session was found, return an InvalidSession error.
+      if (!session) return res.status(404).json(new Errors.InvalidSession());
+
+      // Update the session's name.
+      await session.update({
+        name: req.body.name,
+      });
+
+      logger.debug(
+        `User ${req.user.username} (${req.user.id}) requested to rename user ${user.username} (${user.id})'s session ${session.id} to ${req.body.name}.`,
+      );
+
+      // Return a success.
+      return res
+        .status(200)
+        .json(KVExtractor(session.toJSON(), ['user'], true));
+    } catch (error) {
+      logger.error(error);
+      return res.status(500).json(new Errors.Internal());
+    }
+  },
+);
+
+app.put(
+  // PUT /api/users/me/sessions/:sid/permissions
+  '/api/users/me/sessions/:sid([0-9]{10})/permissions',
+  KillSwitch(KillSwitches.ACCOUNT_MODIFY),
+  ReverifyIdentity(),
+  BodyValidator({
+    flags: 'number',
+  }),
+  async (
+    req: Request<{ sid: string }, null, { flags: number }>,
+    res: Response<
+      Cumulonimbus.Structures.Session | Cumulonimbus.Structures.Error
+    >,
+  ) => {
+    // Find the session with the given ID.
+    const session = await Session.findOne({
+      where: {
+        user: req.user.id,
+        id: req.params.sid,
+      },
+    });
+
+    // If the session that is being updated is a full session
+    // or if the session is the current session, return an InvalidSession error.
+    if (session.permissionFlags === null || session.id === req.session.id)
+      return res.status(400).json(new Errors.InvalidSession());
+
+    // If no session was found, return an InvalidSession error.
+    if (!session) return res.status(404).json(new Errors.InvalidSession());
+
+    // If the session updating this session is a scoped session, do not allow the new session to have more permissions than the session creating it. (No privilege escalation!!)
+    if (req.session.permissionFlags !== null)
+      req.body.flags &= req.session.permissionFlags;
+
+    // Update the session's permission flags.
+    await session.update({
+      permissionFlags: req.body.flags,
+    });
+
+    logger.debug(
+      `User ${req.user.username} (${req.user.id}) requested to update their session ${session.id}'s permissions to ${req.body.flags}. (Previously ${session.permissionFlags})`,
+    );
+
+    // Return a success.
+    return res.status(200).json(KVExtractor(session.toJSON(), ['user'], true));
+  },
+);
+
+app.put(
+  // PUT /api/users/:uid/sessions/:sid/permissions
+  '/api/users/:uid([0-9]{13})/sessions/:sid([0-9]{10})/permissions',
+  SessionChecker(true),
+  SessionPermissionChecker(PermissionFlags.STAFF_MODIFY_SESSIONS),
+  BodyValidator({
+    flags: 'number',
+  }),
+  async (
+    req: Request<{ uid: string; sid: string }, null, { flags: number }>,
+    res: Response<
+      Cumulonimbus.Structures.Session | Cumulonimbus.Structures.Error
+    >,
+  ) => {
+    try {
+      // Find the user with the given ID.
+      const user = await User.findByPk(req.params.uid);
+
+      // If no user was found, return an InvalidUser error.
+      if (!user) return res.status(404).json(new Errors.InvalidUser());
+
+      // Find the session with the given ID.
+      const session = await Session.findOne({
+        where: {
+          user: user.id,
+          id: req.params.sid,
+        },
+      });
+
+      // If no session was found, return an InvalidSession error.
+      if (!session) return res.status(404).json(new Errors.InvalidSession());
+
+      // Update the session's permission flags.
+      await session.update({
+        permissionFlags: req.body.flags,
+      });
+
+      logger.debug(
+        `User ${req.user.username} (${req.user.id}) requested to update user ${user.username} (${user.id})'s session ${session.id}'s permissions to ${req.body.flags}. (Previously ${session.permissionFlags})`,
+      );
+
+      // Return a success.
+      return res
+        .status(200)
+        .json(KVExtractor(session.toJSON(), ['user'], true));
+    } catch (error) {
+      logger.error(error);
+      return res.status(500).json(new Errors.Internal());
+    }
+  },
+);
+
 app.delete(
   // DELETE /api/users/me/sessions/me
   '/api/users/me/sessions/me',
+  KillSwitch(KillSwitches.ACCOUNT_MODIFY),
   SessionChecker(),
   SessionPermissionChecker(PermissionFlags.SESSION_MODIFY),
-  KillSwitch(KillSwitches.ACCOUNT_MODIFY),
   async (
     req: Request,
     res: Response<
@@ -517,7 +711,6 @@ app.delete(
   '/api/users/:uid([0-9]{13})/sessions/:sid([0-9]{10})',
   SessionChecker(true),
   SessionPermissionChecker(PermissionFlags.STAFF_MODIFY_SESSIONS),
-  KillSwitch(KillSwitches.ACCOUNT_MODIFY),
   async (
     req: Request<{ uid: string; sid: string }>,
     res: Response<
@@ -560,9 +753,9 @@ app.delete(
 app.delete(
   // DELETE /api/users/me/sessions
   '/api/users/me/sessions',
+  KillSwitch(KillSwitches.ACCOUNT_MODIFY),
   SessionChecker(),
   SessionPermissionChecker(PermissionFlags.SESSION_MODIFY),
-  KillSwitch(KillSwitches.ACCOUNT_MODIFY),
   BodyValidator({
     ids: new ExtendedValidBodyTypes('array', false, 'string'),
   }),
@@ -648,9 +841,9 @@ app.delete(
 app.delete(
   // Delete /api/users/me/sessions/all
   '/api/users/me/sessions/all',
+  KillSwitch(KillSwitches.ACCOUNT_MODIFY),
   SessionChecker(),
   SessionPermissionChecker(PermissionFlags.SESSION_MODIFY),
-  KillSwitch(KillSwitches.ACCOUNT_MODIFY),
   async (
     req: Request,
     res: Response<
@@ -690,7 +883,6 @@ app.delete(
   '/api/users/:uid([0-9]{13})/sessions/all',
   SessionChecker(),
   SessionPermissionChecker(PermissionFlags.STAFF_MODIFY_SESSIONS),
-  KillSwitch(KillSwitches.ACCOUNT_MODIFY),
   async (
     req: Request<{ uid: string }>,
     res: Response<
